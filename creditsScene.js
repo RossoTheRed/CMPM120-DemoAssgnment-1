370aed7b805d44152f549fd0cb9840e77a1cf8a8
RossoTheRed <4mylittlesqueaky@gmail.com>
2026-04-23 14:49:57 -0700

    Made menu screen interactable

--- creditsScene.js
+++ creditsScene.js
@@ -7,14 +7,6 @@ class CreditScene extends Phaser.Scene {
 	preload() {
 		this.load.path = "assets/";
 
-		this.load.image("GoogleSlides-Logo","GoogleSlides-Logo.png");
-
-		// this.load.atlas("Yoosung", "gifyoosungooh.gif", "gifyoosungooh.JSON");
-		//this.load.image("Yoosung","gifyoosungooh.gif");
-		// this.load.spritesheet("sheet", "ToiletTalk-SpriteSheet.png", { frameWidth: 1080, frameHeight: 754.37});
-		// this.load.audio("ToiletSound", "ToyDuck.mp3");
-
-		// this.load.image("LoadIcon", "LoadIcon.png");
 		this.load.image("LoadIconA", "LoadIcon2_A.png");
 		this.load.image("LoadIconB", "LoadIcon2_B.png");
 		this.load.video("ToiletTalkVid", "ToiletTalkLogoVid.mp4")
@@ -22,13 +14,7 @@ class CreditScene extends Phaser.Scene {
 	}
 
 	create() {
-		this.anims.create({
-			key: "animation",
-			frames: this.anims.generateFrameNumbers("sheet",{frames:[1,3,4,6,7,8,9]}),
-			frameRate: 3,
-			repeat: 0
-		});
-
+		
 		this.ToiletTalk = this.add.video(400,300,"ToiletTalkVid");
 		this.ToiletTalk.setScale(0.9);
 		this.ToiletTalk.alpha = 0;
@@ -88,7 +74,7 @@ class CreditScene extends Phaser.Scene {
 
 
 		this.ToiletTweenIn.play();
-		this.ToiletTalk.play();
+		this.time.delayedCall(this.delayTime, this.ToiletTalk.play, [], this.ToiletTalk);
 		this.ToiletTweenOut.play();
 
 		this.PhaserLogo = this.add.image(400,300,"PhaserLogo");
@@ -99,7 +85,7 @@ class CreditScene extends Phaser.Scene {
 			targets: this.PhaserLogo,
 			persist: true,
 			alpha: { from: 0, to: 1 },
-			delay: 10000,
+			delay: 8000,
 			duration: 1000,
 			ease: "Sine.In",
 			repeat: 0
@@ -107,16 +93,137 @@ class CreditScene extends Phaser.Scene {
 		this.PhaserTweenOut = this.tweens.add({
 			targets: this.PhaserLogo,
 			persist: true,
-			delay: 15000,
+			delay: 12000,
 			alpha: { from: 1, to: 0 },
 			duration: 2000,
 			ease: "Sine.Out",
 			repeat: 0
 		});
 
+		this.tweens.add({
+			targets: [this.loadIconA, this.loadIconB, this.loadText],
+			persist: true,
+			delay: 14000,
+			alpha: { from: 1, to: 0 },
+			duration: 2000,
+			ease: "Sine.Out",
+			repeat: 0
+		});
+
+		this.timer = this.time.delayedCall(1/*6000*/, this.scene.start, ["menuScene"], this.scene);
 	}
 
-	update() {	
+	update() {
+		let mouse = this.input.activePointer;
+		this.textObject.text = `Mouse: ${mouse.x},${mouse.y}\nTime Elapsed: ${Math.trunc(this.timer.elapsed)}`;
+	}
+}
+
+class MenuScene extends Phaser.Scene {
+	constructor() {
+		super("menuScene");
+	}
+
+	preload() {
+		this.load.path = "assets/";
+		this.load.audio("mainBGM","GameMusic.mp3");
+		this.load.audio("menuSelect","MenuSelect.mp3");
+	}
+
+	create() {
+		this.textObject = this.add.text(
+			0,
+			500,
+			"Placeholder",
+			{ font: "40px Arial", color: "#ffffff" }
+		);
+
+		this.graphics = this.add.graphics();
+		this.graphics.fillStyle(0x000000, 1);
+
+		this.bg = this.graphics.fillRect(0, 0, 800, 600);
+		this.bg.alpha = 0;
+		this.sceneIntro = this.tweens.add({
+			targets: this.bg,
+			alpha: {from: 0, to: 1},
+			duration: 2000,
+			ease: "Sine.Out",
+			repeat: 0
+		});
+
+		this.menuBGM = this.sound.add("mainBGM",{repeat: -1});
+		this.sound.stopAll();
+		this.menuBGM.play();
+
+		this.title = this.add.text(400, 75, "SerenaGame", { font: "50px Arial", color: "#ffffff"});
+		this.title.x = 400 - this.title.width/2;
+		this.title.setFontStyle("italic");
+
+
+		this.graphics.lineStyle(5, "0xffffff", 1);
+		this.graphics.lineBetween(this.title.x, this.title.y + this.title.height, (this.title.x + this.title.width), this.title.y + this.title.height);
+
+		this.menuSelect = this.sound.add("menuSelect");
+
+		this.playButton = this.add.text(400, 200, "Play", { font: "50px Arial", color: "#ffffff" });
+		this.playButton.x = 400 - this.playButton.width/2;
+		this.playButton.setInteractive({useHandCursor: true});
+		this.playButton.on("pointerdown", () =>
+		{
+			this.menuSelect.play();
+			this.menuBGM.stop();
+			this.time.delayedCall(500,this.scene.start,["creditScene"],this.scene);
+		});
+		this.playButton.on("pointerover", () => {
+			this.playButton.setFontStyle("italic");
+			this.playButton.setPadding({right: 2});
+			this.playButton.x = 400 - this.playButton.width / 2;
+		});
+		this.playButton.on("pointerout", () => {
+			this.playButton.setFontStyle("");
+			this.playButton.setPadding({right: 0});
+			this.playButton.x = 400 - this.playButton.width / 2;
+		});
+
+		this.settingsButton = this.add.text(400, 300, "Settings", { font: "50px Arial", color: "#ffffff" });
+		this.settingsButton.setInteractive({ cursor: "not-allowed" });
+		this.settingsButton.x = 400 - this.settingsButton.width / 2;
+		this.settingsButton.on("pointerover", () => {
+			this.settingsButton.setFontStyle("italic");
+			this.settingsButton.setPadding({ right: 2 });
+			this.settingsButton.x = 400 - this.settingsButton.width / 2;
+		});
+		this.settingsButton.on("pointerout", () => {
+			this.settingsButton.setFontStyle("");
+			this.settingsButton.setPadding({ right: 0 });
+			this.settingsButton.x = 400 - this.settingsButton.width / 2;
+		});
+
+		this.quitButton = this.add.text(400, 400, "Quit", { font: "50px Arial", color: "#ffffff" });
+		this.quitButton.x = 400 - this.quitButton.width / 2;
+		this.quitButton.setInteractive({ useHandCursor: true });
+		this.quitButton.on("pointerdown", () => {
+			this.menuSelect.play();
+			this.menuBGM.stop();
+			this.time.delayedCall(500, this.scene.start, ["creditScene"], this.scene);
+		});
+		this.quitButton.on("pointerover", () => {
+			this.quitButton.setFontStyle("italic");
+			this.quitButton.setPadding({ right: 2 });
+			this.quitButton.x = 400 - this.playButton.width / 2;
+		});
+		this.quitButton.on("pointerout", () => {
+			this.quitButton.setFontStyle("");
+			this.quitButton.setPadding({ right: 0 });
+			this.quitButton.x = 400 - this.quitButton.width / 2;
+		});
+
+		// this.graphics.lineStyle(5, 0x000000, 0.5);
+		// this.graphics.lineBetween(250, 100, 450, 100);
+		
+	}
+
+	update() {
 		let mouse = this.input.activePointer;
 		this.textObject.text = `Mouse: ${mouse.x},${mouse.y}`;
 	}
@@ -127,7 +234,7 @@ config = {
 	width: 800,
 	height: 600,
 	backgroundColor: 0xFFFFFF,
-	scene: [CreditScene]
+	scene: [CreditScene,MenuScene]
 }
 
 let game = new Phaser.Game(config);
