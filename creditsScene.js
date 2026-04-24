eba698e2839a27d6853fb1431edacd900eed0b06
RossoTheRed <4mylittlesqueaky@gmail.com>
2026-04-24 13:19:50 -0700

    Added a credits scene

--- creditsScene.js
+++ creditsScene.js
@@ -1,7 +1,7 @@
 //http://127.0.0.1:5500/
-class CreditScene extends Phaser.Scene {
+class IntroScene extends Phaser.Scene {
 	constructor() {
-		super("creditScene");
+		super("introScene");
 	}
 		
 	preload() {
@@ -123,7 +123,7 @@ class CreditScene extends Phaser.Scene {
 			ease: "Sine.Out",
 			repeat: 0
 		});
-		this.timer = this.time.delayedCall(2/*20000*/, this.scene.start, ["menuScene"], this.scene);
+		this.timer = this.time.delayedCall(2/*20000*/, this.scene.start, [/*"menuScene"*/"creditScene"], this.scene);
 	}
 
 	update() {
@@ -206,8 +206,45 @@ class MenuScene extends Phaser.Scene {
 			// });
 		});
 
+		//Credits Button
+		this.creditsButton = this.add.text(400, 300, "Project Credits", { font: "50px Arial", color: "#ffffff" });
+		this.creditsButton.x = 400 - this.creditsButton.width / 2;
+		this.creditsButton.setInteractive({ useHandCursor: true });
+		this.creditsButton.on("pointerdown", () => {
+			this.menuSelect.play();
+			this.menuBGM.stop();
+			this.time.delayedCall(500, this.scene.start, ["creditScene"], this.scene);
+		});
+		this.creditsButton.on("pointerover", () => {
+			this.creditsButton.setFontStyle("italic");
+			this.creditsButton.setPadding({ right: 2 });
+			this.creditsButton.x = 400 - this.creditsButton.width / 2;
+
+			// this.tweens.add({
+			// 	targets: this.selectLine,
+			// 	x: this.creditsButton.x,
+			// 	y: this.creditsButton.y + this.creditsButton.height/2,
+			// 	ease: "ease.InOut",
+			// 	displayWidth: this.creditsButton.width
+
+			// });
+		});
+		this.creditsButton.on("pointerout", () => {
+			this.creditsButton.setFontStyle("");
+			this.creditsButton.setPadding({ right: 0 });
+			this.creditsButton.x = 400 - this.creditsButton.width / 2;
+
+			// this.tweens.add({
+			// 	targets: this.selectLine,
+			// 	x: 0, //this.title.x,
+			// 	y: 0, //this.title.y + this.title.height,
+			// 	ease: "ease.InOut",
+			// 	displayWidth: this.title.width
+			// });
+		});
+
 		// Settings Button
-		this.settingsButton = this.add.text(400, 300, "Settings", { font: "50px Arial", color: "#ffffff" });
+		this.settingsButton = this.add.text(400, 400, "Settings", { font: "50px Arial", color: "#ffffff" });
 		this.settingsButton.setInteractive({ cursor: "not-allowed" });
 		this.settingsButton.x = 400 - this.settingsButton.width / 2;
 		this.settingsButton.on("pointerover", () => {
@@ -222,18 +259,18 @@ class MenuScene extends Phaser.Scene {
 		});
 
 		//Quit button
-		this.quitButton = this.add.text(400, 400, "Quit", { font: "50px Arial", color: "#ffffff" });
+		this.quitButton = this.add.text(400, 500, "Quit", { font: "50px Arial", color: "#ffffff" });
 		this.quitButton.x = 400 - this.quitButton.width / 2;
 		this.quitButton.setInteractive({ useHandCursor: true });
 		this.quitButton.on("pointerdown", () => {
 			this.menuSelect.play();
 			this.menuBGM.stop();
-			this.time.delayedCall(500, this.scene.start, ["creditScene"], this.scene);
+			this.time.delayedCall(500, this.scene.start, ["introScene"], this.scene);
 		});
 		this.quitButton.on("pointerover", () => {
 			this.quitButton.setFontStyle("italic");
 			this.quitButton.setPadding({ right: 2 });
-			this.quitButton.x = 400 - this.playButton.width / 2;
+			this.quitButton.x = 400 - this.quitButton.width / 2;
 		});
 		this.quitButton.on("pointerout", () => {
 			this.quitButton.setFontStyle("");
@@ -360,6 +397,120 @@ class StoryScene extends Phaser.Scene {
 
 	update() {	}
 }
+class CreditScene extends Phaser.Scene {
+	constructor() {
+		super("creditScene");
+	}
+
+	preload() {
+		this.load.path = "assets/";
+		this.load.audio("mainBGM", "GameMusic.mp3");
+		this.load.audio("menuSelect", "MenuSelect.mp3");
+		this.load.image("musicIcon","musicNoteIcon.png");
+		this.load.image("soundIcon", "soundIcon.png");
+		this.load.image("toiletLogo", "ToiletTalk-LogoCropped.png");
+		this.load.image("phaserLogo", "phaser4-logo.png");
+	}
+
+	create() {
+		this.graphics = this.add.graphics();
+
+		this.menuBGM = this.sound.add("mainBGM", { repeat: -1 });
+		this.sound.stopAll();
+		this.menuBGM.play();
+
+		this.title = this.add.text(400, 25, "Project Credits", { font: "50px Arial", color: "#ffffff" });
+		this.title.x = 400 - this.title.width / 2;
+		this.title.setFontStyle("italic");
+
+
+		this.graphics.lineStyle(5, "0xffffff", 1);
+		this.selectLine = this.graphics.lineBetween(this.title.x, this.title.y + this.title.height, (this.title.x + this.title.width), this.title.y + this.title.height);
+		//this.selectLine.setOrigin(this.title.x, this.title.y + this.title.height);
+
+		this.menuSelect = this.sound.add("menuSelect");
+
+		this.musicIcon1 = this.add.image(100,150,"musicIcon");
+		this.musicText1 = this.make.text({
+			x: this.musicIcon1.x + this.musicIcon1.width/2,
+			y: this.musicIcon1.y - this.musicIcon1.height/2,
+			text: "Menu music \"Video Game Menu Music\" by magmadiverrr on FreeSound.org",
+			style: { font: "20px Arial", fill: "#FFFFFF", wordWrap: { width: 300 }, align: "center" }
+		});
+
+		this.musicIcon2 = this.add.image(450, 150, "musicIcon");
+		this.musicText2 = this.make.text({
+			x: this.musicIcon2.x + this.musicIcon2.width/2,
+			y: this.musicIcon2.y - this.musicIcon2.height / 2,
+			text: "Story music \"Once Upon A Time\" by Toby Fox from Undertale",
+			style: { font: "20px Arial", fill: "#FFFFFF", wordWrap: { width: 300 }, align: "center" }
+		});
+
+		this.soundIcon1 = this.add.image(100, 250, "soundIcon");
+		this.soundText1 = this.make.text({
+			x: this.soundIcon1.x + this.soundIcon1.width/2,
+			y: this.soundIcon1.y - this.soundIcon1.height / 2,
+			text: "Menu select sound \"Menu Select\" by Pumodi on FreeSound.org",
+			style: { font: "20px Arial", fill: "#FFFFFF", wordWrap: { width: 300 }, align: "center" }
+		});
+
+		this.soundIcon2 = this.add.image(450, 250, "soundIcon");
+		this.soundText2 = this.make.text({
+			x: this.soundIcon2.x + this.soundIcon2.width / 2,
+			y: this.soundIcon2.y - this.soundIcon2.height / 2,
+			text: "Toilet Quack 1 \"TOONAnml-Blue Snowball Microphone, CU_Duck Whistle, Single Quack 02_Nicholas Judy_TDC\" by designerschoice on FreeSound.org",
+			style: { font: "20px Arial", fill: "#FFFFFF", wordWrap: { width: 300 }, align: "center" }
+		});
+		
+		this.soundIcon3 = this.add.image(100, 350, "soundIcon");
+		this.soundText3 = this.make.text({
+			x: this.soundIcon3.x + this.soundIcon3.width / 2,
+			y: this.soundIcon3.y - this.soundIcon3.height / 2,
+			text: "Toilet Quack 2 \"Quack_Fart_Noise_44K\" by stereostory on FreeSound.org",
+			style: { font: "20px Arial", fill: "#FFFFFF", wordWrap: { width: 300 }, align: "center" }
+		});
+
+		this.logoIcon1 = this.add.image(100, 450, "toiletLogo");
+		this.logoIcon1.setScale(0.1)
+		this.logoText1 = this.make.text({
+			x: this.logoIcon1.x + 40,// + this.logoIcon1.width / 2,
+			y: this.logoIcon1.y,// - this.logoIcon1.height / 2,
+			text: "Toilet Talk Logo by my friend Curren",
+			style: { font: "20px Arial", fill: "#FFFFFF", wordWrap: { width: 300 }, align: "center" }
+		});
+
+		this.logoIcon2 = this.add.image(600, 450, "phaserLogo");
+		this.logoIcon2.setScale(0.2)
+		this.logoText2 = this.make.text({
+			x: this.logoIcon2.x - 100,// - this.logoIcon2.width / 2,
+			y: this.logoIcon2.y + 40,// - this.logoIcon1.height / 2,
+			text: "Phaser Logo by Phaser",
+			style: { font: "20px Arial", fill: "#FFFFFF", wordWrap: { width: 300 }, align: "center" }
+		});
+
+		//Quit button
+		this.quitButton = this.add.text(0, 20, "Back", { font: "30px Arial", color: "#ffffff" });
+		this.quitButton.x = 0 + this.quitButton.width / 2;
+		this.quitButton.setInteractive({ useHandCursor: true });
+		this.quitButton.on("pointerdown", () => {
+			this.menuSelect.play();
+			this.menuBGM.stop();
+			this.time.delayedCall(500, this.scene.start, ["menuScene"], this.scene);
+		});
+		this.quitButton.on("pointerover", () => {
+			this.quitButton.setFontStyle("italic");
+			this.quitButton.setPadding({ right: 2 });
+			this.quitButton.x = 0 + this.quitButton.width / 2;
+		});
+		this.quitButton.on("pointerout", () => {
+			this.quitButton.setFontStyle("");
+			this.quitButton.setPadding({ right: 0 });
+			this.quitButton.x = 0 + this.quitButton.width / 2;
+		});
+	}
+
+	update() { }
+}
 
 /*class EmptyScene extends Phaser.Scene {
 	constructor() {
@@ -378,7 +529,7 @@ config = {
 	width: 800,
 	height: 600,
 	backgroundColor: 0x000000,
-	scene: [CreditScene,MenuScene,StoryScene]
+	scene: [IntroScene,MenuScene,StoryScene,CreditScene]
 }
 
 let game = new Phaser.Game(config);
